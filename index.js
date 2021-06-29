991f552b1af7fa6296f7b34723bee64499ab3dfd
cmalven <chris@malven.co>
2021-06-29 15:46:05 -0500

    Adds "function-calc-no-invalid"

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ module.exports = {
     "font-family-no-duplicate-names": true,
     "font-weight-notation": "named-where-possible",
     "function-blacklist": null,
+    "function-calc-no-invalid": true,
     "function-calc-no-unspaced-operator": true,
     "function-comma-newline-after": null,
     "function-comma-newline-before": "never-multi-line",
